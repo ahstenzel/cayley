eb09f862561768032842b891402e56282d75257b
ahstenzel <38267918+ahstenzel@users.noreply.github.com>
2021-10-15 19:16:49 -0500

    Changed definitions of transform functions

--- include/cayley/detail/transforms.h
+++ include/cayley/detail/transforms.h
@@ -6,9 +6,9 @@
 #include "cayley/detail/vec4.h"
 #include "cayley/detail/mat4.h"
 
-mat4 matrix_translate(mat4 m, vec3 v);
-mat4 matrix_rotate(mat4 m, float a, vec3 v);
-mat4 matrix_scale(mat4 m, vec3 s);
+mat4 matrix_translate(vec3 v);
+mat4 matrix_rotate(float a, vec3 v);
+mat4 matrix_scale(vec3 s);
 mat4 matrix_ortho(float left, float right, float near, float far, float top, float bottom);
 mat4 matrix_perspecive(float fov, float aspect, float near, float far);
 
--- src/transforms.c
+++ src/transforms.c
@@ -3,15 +3,15 @@
 #endif
 #include "cayley/detail/transforms.h"
 
-mat4 matrix_translate(mat4 m, vec3 v) {
+mat4 matrix_translate(vec3 v) {
   mat4 t = mat4_identity;
   t.M4X4_X4 = v.X;
   t.M4X4_Y4 = v.Y;
   t.M4X4_Z4 = v.Z;
-  return mat4_multiply(t, m);
+  return t;
 }
 
-mat4 matrix_rotate(mat4 m, float a, vec3 v) {
+mat4 matrix_rotate(float a, vec3 v) {
   mat4 t = mat4_identity;
   float c = (float)cos(a);
   float s = (float)sin(a);
@@ -24,42 +24,42 @@ mat4 matrix_rotate(mat4 m, float a, vec3 v) {
   t.M4X4_X3 = (v.X * v.Z * (1.0f - c)) + (v.Y * s);
   t.M4X4_Y3 = (v.Y * v.Z * (1.0f - c)) - (v.X * s);
   t.M4X4_Z3 = c + (v.Z * v.Z * (1.0f - c));
-  return mat4_multiply(t, m);
+  return t;
 }
 
-mat4 matrix_scale(mat4 m, vec3 s) {
+mat4 matrix_scale(vec3 s) {
   mat4 t = mat4_identity;
   t.M4X4_X1 = s.X;
   t.M4X4_Y2 = s.Y;
   t.M4X4_Z3 = s.Z;
-  return mat4_multiply(t, m);
+  return t;
 }
 
 mat4 matrix_ortho(float left, float right, float near, float far, float top, float bottom) {
-  mat4 m = mat4_identity;
-  m.M4X4_X1 = 2.0f / (right - left);
-  m.M4X4_Y2 = 2.0f / (top - bottom);
-  m.M4X4_Z3 = -2.0f / (far - near);
-  m.M4X4_X4 = (right + left) / (right - left);
-  m.M4X4_Y4 = (top + bottom) / (top - bottom);
-  m.M4X4_Z4 = (far + near) / (far - near);
-  return m;
+  mat4 t = mat4_identity;
+  t.M4X4_X1 = 2.0f / (right - left);
+  t.M4X4_Y2 = 2.0f / (top - bottom);
+  t.M4X4_Z3 = -2.0f / (far - near);
+  t.M4X4_X4 = (right + left) / (right - left);
+  t.M4X4_Y4 = (top + bottom) / (top - bottom);
+  t.M4X4_Z4 = (far + near) / (far - near);
+  return t;
 }
 
 mat4 matrix_perspecive(float fov, float aspect, float near, float far) {
-  mat4 m = mat4_identity;
+  mat4 t = mat4_identity;
   float top = (float)tan(fov / 2.0f) * near;
   float bottom = -top;
   float right = top * aspect;
   float left = bottom * aspect;
 
-  m.M4X4_X1 = (2.0f * near) / (right - left);
-  m.M4X4_Y2 = (2.0f * near) / (top - bottom);
-  m.M4X4_X3 = (right + left) / (right - left);
-  m.M4X4_Y3 = (top + bottom) / (top - bottom);
-  m.M4X4_Z3 = -(far + near) / (far - near);
-  m.M4X4_W3 = -1.0f;
-  m.M4X4_Z4 = (-2.0f * far * near) / (far - near);
-  m.M4X4_W4 = 0;
-  return m;
+  t.M4X4_X1 = (2.0f * near) / (right - left);
+  t.M4X4_Y2 = (2.0f * near) / (top - bottom);
+  t.M4X4_X3 = (right + left) / (right - left);
+  t.M4X4_Y3 = (top + bottom) / (top - bottom);
+  t.M4X4_Z3 = -(far + near) / (far - near);
+  t.M4X4_W3 = -1.0f;
+  t.M4X4_Z4 = (-2.0f * far * near) / (far - near);
+  t.M4X4_W4 = 0;
+  return t;
 }
